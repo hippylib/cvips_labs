a2d79e59e5216a5a7c2cfedc83bf7cb654e4ad30
Umberto Villa <uvilla@oden.utexas.edu>
2023-03-14 12:56:55 -0500

    Update test_all.sh

--- .github/workflows/test_all.sh
+++ .github/workflows/test_all.sh
@@ -1,3 +1,3 @@
-#!bash
+#!/bin/bash
 
 find . -name '*.ipynb' -exec jupyter nbconvert --ExecutePreprocessor.kernel_name="python3" --ExecutePreprocessor.timeout=1200 --to html --output-dir=out --execute {} \;
